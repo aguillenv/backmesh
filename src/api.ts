0ef37579cd7b9fbafa29f966f74af770818fbb87
L Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2024-08-01 17:30:55 -0700

    specific openai proxy is working

--- src/api.ts
+++ src/api.ts
@@ -10,6 +10,6 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response(`Hello ${env.MY_VARIABLE}!`);
+		return new Response(`Hello Api!`);
 	},
 };
--- src/proxy.ts
+++ src/proxy.ts
@@ -9,7 +9,76 @@
  */
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello Proxy!');
+
+	async verifyFirebaseToken(env: Env, idToken: string) {
+		const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${env.FIREBASE_KEY}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				idToken
+			})
+		});
+
+		if (!response.ok) {
+			console.error('Error verifying ID token:', response.statusText);
+			return false;
+		}
+
+		// TODO import firebase types
+		const data = await response.json() as { users?: any[] }
+		return data && data.users && data.users.length > 0;
 	},
+
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		// Extract the ID token from the Authorization header
+		const authHeader = request.headers.get('Authorization');
+		if (!authHeader || !authHeader.startsWith('Bearer ')) {
+			return new Response('Missing or invalid Authorization header', { status: 401 });
+		}
+
+		const idToken = authHeader.split(' ')[1];
+		const isValid = await this.verifyFirebaseToken(env, idToken);
+
+		if (!isValid) {
+			return new Response('Invalid Firebase ID token', { status: 401 });
+		}
+
+		const requestUrl = new URL(request.url);
+		const parts = requestUrl.pathname.split('/').filter(part => part);
+		// Remove the first part
+		if (parts.length > 0) {
+			parts.shift();
+		}
+		const pathName = parts.join('/');
+		const apiUrl = 'https://api.openai.com/' + pathName;
+
+		const init = {
+			method: request.method,
+			headers: {
+				'Authorization': `Bearer ${env.OPENAI_KEY}`,
+				'Content-Type': 'application/json',
+			},
+			body: await request.clone().text(),
+		};
+
+		const response = await fetch(apiUrl, init);
+
+		if (!response.body) {
+			return new Response('No body in response', { status: 500 });
+		}
+
+		let { readable, writable } = new TransformStream({
+			transform(chunk, controller) {
+				controller.enqueue(chunk);
+			}
+		});
+
+		// Start pumping the body. NOTE: No await!
+		response.body.pipeTo(writable);
+
+		// ... and deliver our Response while that’s running.
+		return new Response(readable, response);
+	}
 };
