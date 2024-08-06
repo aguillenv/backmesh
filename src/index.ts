f4cf969600f546b50f265c969c05363d2f15382d
L Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2024-08-05 21:14:41 -0700

    rename to crud to avoid confusion

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import api from './api';
+import crud from './crud';
 import proxy from './proxy';
 
 export default {
   async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
     const path = url.pathname;
-    if (path.startsWith('/api')) {
-      return api.fetch(request, env, ctx);
-    } else if (path.startsWith('/proxy')) {
+    if (path.startsWith('/v1/crud')) {
+      return crud.fetch(request, env, ctx);
+    } else if (path.startsWith('/v1/proxy')) {
       return proxy.fetch(request, env, ctx);
     }
     return new Response('Not Found', { status: 404 });
