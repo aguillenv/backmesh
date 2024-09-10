389ff54ac336facad2df2e2101bc70599c17d01f
L. Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2024-09-10 16:13:37 -0700

    test user summaries from reqs (#27)

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -201,7 +201,7 @@ describe('Firebase + Gemini API Proxy Failed Creations', () => {
 	});
 });
 
-describe('Firebase + OpenAI API Proxy user access control for files', () => {
+describe('Firebase + OpenAI API Proxy: user access control for files', () => {
 	let fileId1stUser: string, fileId2ndUser: string;
 	beforeAll(async () => {
 		response = await SELF.fetch(`https://example.com/v1/crud/${testUserId}`, {
@@ -752,7 +752,180 @@ describe('Firebase + Gemini API Proxy user access control for files', () => {
 	});
 });
 
-describe('Firebase + OpenAI Proxy user access control for threads', async () => {
+describe('Firebase + Gemini Proxy: completion + summary', async () => {
+	beforeAll(async () => {
+		// create proxy
+		response = await SELF.fetch(`https://example.com/v1/crud/${testUserId}`, {
+			method: 'POST',
+			headers: {
+				Authorization: testUserJwt,
+			},
+			body: geminiProxyInit,
+		});
+		expect(response.status).toBe(200);
+		let data = (await response.json()) as ApiProxy;
+		expect(data.id.length).toBeGreaterThan(0);
+		expect(data.proxyUrl.length).toBeGreaterThan(0);
+		expect(data.apiPrivateKey === '').toBe(true);
+		proxyId = data.id;
+		reqHeader = JSON.parse(geminiProxyInit)['apiReqHeader'];
+	});
+
+	it('empty summary', async () => {
+		response = await SELF.fetch(
+			`https://example.com/v1/crud/${testUserId}/${proxyId!}/summary`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: testUserJwt,
+				},
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+		const body: EndUserAnalyticsSummary[] = await response.json();
+		expect(body.length).toBe(0);
+	});
+
+	it('chat completion endpoint', async () => {
+		response = await SELF.fetch(
+			`https://example.com/v1/proxy/${testUserId}/${proxyId!}/v1beta/models/gemini-1.5-flash:generateContent`,
+			{
+				method: 'POST',
+				headers: {
+					[reqHeader]: testUser1stUserJwt,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					contents: [
+						{
+							role: 'user',
+							parts: [
+								{
+									text: 'I have two dogs in my house. How many paws are in my house?',
+								},
+							],
+						},
+					],
+				}),
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+
+		// one user in summary
+		response = await SELF.fetch(
+			`https://example.com/v1/crud/${testUserId}/${proxyId!}/summary`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: testUserJwt,
+				},
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+		const body: EndUserAnalyticsSummary[] = await response.json();
+		if (body.length !== 1) console.error(body);
+		expect(body.length).toBe(1);
+		expect(body[0].endUserId).toBe(testUser1stUserId);
+		expect(body[0].totalCost).toBeGreaterThan(0);
+		expect(body[0].totalTiming).toBeGreaterThan(0);
+		expect(body[0].errorCount).toBe(0);
+		expect(body[0].reqCount).toBe(1);
+		expect(body[0].firstTs).toBeGreaterThan(0);
+		expect(body[0].lastTs).toBe(body[0].lastTs);
+	});
+});
+
+describe('Firebase + OpenAI Proxy: completion + summary', async () => {
+	beforeAll(async () => {
+		// create proxy
+		response = await SELF.fetch(`https://example.com/v1/crud/${testUserId}`, {
+			method: 'POST',
+			headers: {
+				Authorization: testUserJwt,
+			},
+			body: openAIProxyInit,
+		});
+		expect(response.status).toBe(200);
+		let data = (await response.json()) as ApiProxy;
+		expect(data.id.length).toBeGreaterThan(0);
+		expect(data.proxyUrl.length).toBeGreaterThan(0);
+		expect(data.apiPrivateKey === '').toBe(true);
+		proxyId = data.id;
+		reqHeader = JSON.parse(openAIProxyInit)['apiReqHeader'];
+	});
+
+	it('empty summary', async () => {
+		response = await SELF.fetch(
+			`https://example.com/v1/crud/${testUserId}/${proxyId!}/summary`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: testUserJwt,
+				},
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+		const body: EndUserAnalyticsSummary[] = await response.json();
+		expect(body.length).toBe(0);
+	});
+
+	it('chat completion endpoint', async () => {
+		response = await SELF.fetch(
+			`https://example.com/v1/proxy/${testUserId}/${proxyId!}/v1/chat/completions`,
+			{
+				method: 'POST',
+				headers: {
+					[reqHeader]: `Bearer ${testUser1stUserJwt}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					model: 'gpt-4o-mini',
+					messages: [
+						{
+							role: 'system',
+							content: 'You are a helpful assistant.',
+						},
+						{
+							role: 'user',
+							content: 'Hello!',
+						},
+					],
+				}),
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+
+		// one user in summary
+		response = await SELF.fetch(
+			`https://example.com/v1/crud/${testUserId}/${proxyId!}/summary`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: testUserJwt,
+				},
+			},
+		);
+		if (response.status !== 200) console.error(await response.text());
+		expect(response.status).toBe(200);
+		const body: EndUserAnalyticsSummary[] = await response.json();
+		if (body.length !== 1) console.error(body);
+		expect(body.length).toBe(1);
+		expect(body[0].endUserId).toBe(testUser1stUserId);
+		expect(body[0].totalCost).toBeGreaterThan(0);
+		expect(body[0].totalTiming).toBeGreaterThan(0);
+		expect(body[0].errorCount).toBe(0);
+		expect(body[0].reqCount).toBe(1);
+		expect(body[0].firstTs).toBeGreaterThan(0);
+		expect(body[0].lastTs).toBe(body[0].lastTs);
+	});
+});
+
+describe('Firebase + OpenAI Proxy: user access control for threads', async () => {
 	let threadId1stUser: string, threadId2ndUser: string;
 	beforeAll(async () => {
 		// create proxy
@@ -1116,7 +1289,7 @@ describe('Firebase + Gemini API Proxy Rate Limit', () => {
 	});
 });
 
-describe('Firebase + Anthropic API Proxy Basic usage', () => {
+describe('Firebase + Anthropic API Proxy Completion usage', () => {
 	const messageBody = JSON.stringify({
 		model: 'claude-3-5-sonnet-20240620',
 		max_tokens: 1024,
