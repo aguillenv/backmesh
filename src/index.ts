60f7b21cae123d76d64ab88aa4d4aa8ad94aaea9
lfdepombo <2381303+depombo@users.noreply.github.com>
2024-10-09 10:18:12 -0700

    add highlight error handling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { H } from '@highlight-run/cloudflare'
+
 import crud from './crud';
 import proxy from './proxy';
 
@@ -43,30 +45,37 @@ function handleOptions(request: Request) {
 
 export default {
 	async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
-		if (request.method === 'OPTIONS') {
-			return handleOptions(request);
-		}
-		const url = new URL(request.url);
-		const path = url.pathname;
+		H.init(request, { HIGHLIGHT_PROJECT_ID: 'lgxpl54d' }, ctx);
 		let resp;
-		if (path.startsWith('/v1/crud')) {
-			resp = await crud.fetch(request, env);
-		} else if (path.startsWith('/v1/proxy')) {
-			const proxyReq = await proxy.validate(request, env);
-			const start = performance.now();
-			const proxyResp = await proxy.fetch(proxyReq, env);
-			const end = performance.now();
-			const timing = end - start;
-			// log request without blocking response if request was valid
-			ctx.waitUntil(proxy.postprocessing(proxyReq, proxyResp, start, timing, env));
-			resp = proxyResp.response;
+		try {
+			if (request.method === 'OPTIONS') {
+				return handleOptions(request);
+			}
+			const url = new URL(request.url);
+			const path = url.pathname;
+			if (path.startsWith('/v1/crud')) {
+				resp = await crud.fetch(request, env);
+			} else if (path.startsWith('/v1/proxy')) {
+				const proxyReq = await proxy.validate(request, env);
+				const start = performance.now();
+				const proxyResp = await proxy.fetch(proxyReq, env);
+				const end = performance.now();
+				const timing = end - start;
+				// log request without blocking response if request was valid
+				ctx.waitUntil(proxy.postprocessing(proxyReq, proxyResp, start, timing, env));
+				resp = proxyResp.response;
+			}
+			if (resp === undefined) return new Response('Not Found', { status: 404 });
+			resp.headers.set('Access-Control-Allow-Origin', '*');
+			resp.headers.set(
+				'Access-Control-Allow-Methods',
+				'GET, POST, PUT, DELETE, OPTIONS',
+			);
+			H.sendResponse(resp);
+			return resp;
+		} catch(e: any) {
+			H.consumeError(e);
+			return resp || new Response(e, {status: 500});
 		}
-		if (resp === undefined) return new Response('Not Found', { status: 404 });
-		resp.headers.set('Access-Control-Allow-Origin', '*');
-		resp.headers.set(
-			'Access-Control-Allow-Methods',
-			'GET, POST, PUT, DELETE, OPTIONS',
-		);
-		return resp;
 	},
 };
